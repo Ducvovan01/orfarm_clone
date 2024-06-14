bd5a18879adcafa2097ffb8ceb9f443a7696cd7b
DucLangTu <77925620+ducvo177@users.noreply.github.com>
2024-06-14 13:52:04 +0700

    notfound

--- orfarm/src/router/index.js
+++ orfarm/src/router/index.js
@@ -1,16 +1,75 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import App from '../App.vue';
+import ShopDetails from '../views/ShopDetails.vue';
+import ShopLeftSidebar from '../views/ShopLeftSidebar.vue';
+import Blog from '../views/Blog.vue';
+import BlogDetails from '../views/BlogDetails.vue';
+import Checkout from '../views/Checkout.vue';
+import LogIn from '../views/LogIn.vue';
+import Cart from '../views/Cart.vue';
+import Wishlist from '../views/Wishlist.vue';
 import NotFound from '../views/NotFound.vue';
 
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: App
+  },
+  {
+    path: '/shop-details',
+    name: 'shop-details',
+    component: ShopDetails
+  },
+  {
+    path: '/shop-left-sidebar',
+    name: 'shop-left-sidebar',
+    component: ShopLeftSidebar
+  },
+  {
+    path: '/blog',
+    name: 'blog',
+    component: Blog
+  },
+  {
+    path: '/blog-details',
+    name: 'blog-details',
+    component: BlogDetails
+  },
+  {
+    path: '/checkout',
+    name: 'checkout',
+    component: Checkout
+  },
+  {
+    path: '/log-in',
+    name: 'log-in',
+    component: LogIn
+  },
+  {
+    path: '/cart',
+    name: 'cart',
+    component: Cart
+  },
+  {
+    path: '/wishlist',
+    name: 'wishlist',
+    component: Wishlist
+  },
+  {
+    path: '/404',
+    name: 'not-found',
+    component: NotFound
+  },
+  {
+    path: '/:catchAll(.*)',
+    redirect: '/404'
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/404',
-      name: 'notfound',
-      component: NotFound      
-    }
-  ]
-})
+  routes
+});
 
-export default router
+export default router;
--- orfarm/src/views/NotFound.vue
+++ orfarm/src/views/NotFound.vue
@@ -1,92 +1,73 @@
 <script setup>
-import Footer from '@/components/Footer.vue';
-import Header from '@/components/Header.vue';
-
 
 </script>
 
 <template>
-    <Header></Header>
-    ,,,ssssssssssssssssssssss
-    <br/>
-    cc
-    ,,,ssssssssssssssssssssss
-    <br/>
-    cc
-    ,,,ssssssssssssssssssssss
-    <br/>
-    cc
-    ,,,ssssssssssssssssssssss
-    <br/>
-    cc
-    ,,,ssssssssssssssssssssss
-    <br/>
-    cc
-    ,,,ssssssssssssssssssssss
-    <br/>
-    cc
-    ,,,ssssssssssssssssssssss
-    <br/>
-    cc
-    ,,,ssssssssssssssssssssss
-    <br/>
-    cc
-    ,,,ssssssssssssssssssssss
-    <br/>
-    cc  ,,,ssssssssssssssssssssss
-    <br/>
-    cc  ,,,ssssssssssssssssssssss
-    <br/>
-    cc  ,,,ssssssssssssssssssssss
-    <br/>
-    cc  ,,,ssssssssssssssssssssss
-    <br/>
-    cc  ,,,ssssssssssssssssssssss
-    <br/>
-    cc  ,,,ssssssssssssssssssssss
-    <br/>
-    cc  ,,,ssssssssssssssssssssss
-    <br/>
-    cc  ,,,ssssssssssssssssssssss
-    <br/>
-    cc  ,,,ssssssssssssssssssssss
-    <br/>
-    cc  ,,,ssssssssssssssssssssss
-    <br/>
-    cc  ,,,ssssssssssssssssssssss
-    <br/>
-    cc  ,,,ssssssssssssssssssssss
-    <br/>
-    cc  ,,,ssssssssssssssssssssss
-    <br/>
-    cc  ,,,ssssssssssssssssssssss
-    <br/>
-    cc  ,,,ssssssssssssssssssssss
-    <br/>
-    cc  ,,,ssssssssssssssssssssss
-    <br/>
-    cc  ,,,ssssssssssssssssssssss
-    <br/>
-    cc  ,,,ssssssssssssssssssssss
-    <br/>
-    cc  ,,,ssssssssssssssssssssss
-    <br/>
-    cc  ,,,ssssssssssssssssssssss
-    <br/>
-    cc  ,,,ssssssssssssssssssssss
-    <br/>
-    cc  ,,,ssssssssssssssssssssss
-    <br/>
-    cc  ,,,ssssssssssssssssssssss
-    <br/>
-    cc  ,,,ssssssssssssssssssssss
-    <br/>
-    cc  ,,,ssssssssssssssssssssss
-    <br/>
-    cc
-    <Footer></Footer>
+    <section class="error-area pt-80 pb-80">
+        <div class="container">
+           <div class="row justify-content-center">
+              <div class="col-xxl-6 col-lg-8 col-md-11">
+                 <div class="tperror__wrapper text-center">
+                    <div class="tperror__thumb p-relative mb-55">
+                       <img src="../assets/img/shape/erorr-bg.png" alt="">
+                       <div class="tperror__shape">
+                          <img src="../assets/img/shape/erorr-shape.png" alt="">
+                       </div>
+                    </div>
+                    <div class="tperror__content">
+                       <h4 class="tperror__title mb-25">Oops...That link is broken.</h4>
+                       <p>Sorry for the inconvenience. Go to our homepage or check out our latest collections.</p>
+                       <div class="tperror__btn">
+                          <a href="index.html">Back to homepage</a>
+                       </div>
+                    </div>
+                 </div>
+              </div>
+           </div>
+        </div>
+     </section>
 </template>
 
 <style scoped>
-
+.tperror__thumb > img {
+    animation: left-right 3s infinite linear;
+    max-width: 100%;
+}
+.tperror__shape {
+    position: absolute;
+    bottom: -22px;
+    left: 50%;
+    transform: translateX(-60%);
+    z-index: -1;
+}
+.tperror__shape img {
+    max-width: 100%;
+}
+.tperror__title {
+    font-family: "Quicksand";
+    font-weight: 700;
+    font-size: 48px;
+    line-height: 40px;
+    letter-spacing: -0.03em;
+    color: var(--tp-heading-primary);
+}
+.tperror__content p {
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 23px;
+    color: var(--tp-text-body);
+    margin-bottom: 45px;
+}
+.tperror__btn a {
+    font-weight: 600;
+    font-size: 13px;
+    line-height: 19px;
+    text-align: center;
+    text-transform: uppercase;
+    color: var(--tp-common-white);
+    background-color: var(--tp-heading-secondary);
+    display: inline-block;
+    padding: 16px 35px;
+    border-radius: 30px;
+}
 </style>
