254699a7bab8f079d7c63b1170fcdcc8aaae20d7
DucLangTu <77925620+ducvo177@users.noreply.github.com>
2024-06-14 15:21:49 +0700

    format_code

--- orfarm/src/views/LogIn.vue
+++ orfarm/src/views/LogIn.vue
@@ -97,116 +97,116 @@ const breadCrumbPath = [{ route: '/', name: 'Trang chủ' }, { name: 'Đăng Nh�
   </section>
 </template>
 
-  <style scoped>
-    .tptrack__content {
-      padding: 50px;
-      border-radius: 10px;
-    }
-    .tptrack__item-icon {
-      flex: 0 0 auto;
-      height: 50px;
-      width: 50px;
-      text-align: center;
-      line-height: 50px;
-      background-color: var(--tp-common-white);
-      border-radius: 6px;
-      margin-right: 20px;
-    }
+<style scoped>
+.tptrack__content {
+  padding: 50px;
+  border-radius: 10px;
+}
+.tptrack__item-icon {
+  flex: 0 0 auto;
+  height: 50px;
+  width: 50px;
+  text-align: center;
+  line-height: 50px;
+  background-color: var(--tp-common-white);
+  border-radius: 6px;
+  margin-right: 20px;
+}
 
-    .tptrack__item-title {
-      font-size: 18px;
-      font-weight: 600;
-      color: var(--tp-heading-primary);
-    }
-    .tptrack__item-content p {
-      font-size: 14px;
-      color: var(--tp-text-body);
-      line-height: 24px;
-      margin-bottom: 0;
-    }
+.tptrack__item-title {
+  font-size: 18px;
+  font-weight: 600;
+  color: var(--tp-heading-primary);
+}
+.tptrack__item-content p {
+  font-size: 14px;
+  color: var(--tp-text-body);
+  line-height: 24px;
+  margin-bottom: 0;
+}
 
-    .tptrack__id form,
-    .tptrack__email form {
-      position: relative;
-    }
-    .tptrack__id span,
-    .tptrack__email span {
-      position: absolute;
-      top: 50%;
-      transform: translateY(-50%);
-      left: 30px;
-      font-size: 16px;
-    }
+.tptrack__id form,
+.tptrack__email form {
+  position: relative;
+}
+.tptrack__id span,
+.tptrack__email span {
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  left: 30px;
+  font-size: 16px;
+}
 
-    .tptrack__id form input,
-    .tptrack__email form input {
-      width: 100%;
-      height: 60px;
-      border: none;
-      background-color: var(--tp-common-white);
-      padding: 10px 60px;
-      border-radius: 6px;
-    }
-    .fa-user-unlock:before {
-      content: '\f2f6';
-      font: normal normal normal 18px/1 FontAwesome;
-    }
-    .fa-user:before {
-      content: '\f007';
-      font: normal normal normal 14px/1 FontAwesome;
-    }
-    .fa-key:before {
-      content: '\f084';
-      font: normal normal normal 14px/1 FontAwesome;
-    }
+.tptrack__id form input,
+.tptrack__email form input {
+  width: 100%;
+  height: 60px;
+  border: none;
+  background-color: var(--tp-common-white);
+  padding: 10px 60px;
+  border-radius: 6px;
+}
+.fa-user-unlock:before {
+  content: '\f2f6';
+  font: normal normal normal 18px/1 FontAwesome;
+}
+.fa-user:before {
+  content: '\f007';
+  font: normal normal normal 14px/1 FontAwesome;
+}
+.fa-key:before {
+  content: '\f084';
+  font: normal normal normal 14px/1 FontAwesome;
+}
 
-    .fa-lock:before {
-      content: '\f023';
-      font: normal normal normal 18px/1 FontAwesome;
-    }
+.fa-lock:before {
+  content: '\f023';
+  font: normal normal normal 18px/1 FontAwesome;
+}
 
-    .fa-envelope:before {
-      content: '\f0e0';
-      font: normal normal normal 14px/1 FontAwesome;
-    }
-    .fa-key:before {
-      content: '\f084';
-      font: normal normal normal 14px/1 FontAwesome;
-    }
-    .fa-long-arrow-right:before {
-      content: '\f178';
-      font: normal normal normal 14px/1 FontAwesome;
-    }
+.fa-envelope:before {
+  content: '\f0e0';
+  font: normal normal normal 14px/1 FontAwesome;
+}
+.fa-key:before {
+  content: '\f084';
+  font: normal normal normal 14px/1 FontAwesome;
+}
+.fa-long-arrow-right:before {
+  content: '\f178';
+  font: normal normal normal 14px/1 FontAwesome;
+}
 
-    .tptrack__submition {
-      background: var(--tp-heading-primary);
-      border-radius: 6px;
-      color: var(--tp-common-white);
-      display: inline-block;
-      font-size: 16px;
-      font-weight: 600;
-      line-height: 1;
-      margin-bottom: 0;
-      padding: 22px 50px;
-      text-align: center;
-      touch-action: manipulation;
-      transition: all 0.3s ease 0s;
-      vertical-align: middle;
-      white-space: nowrap;
-      width: 100%;
-    }
-    .tptrack__submition:hover,
-    .tptrack__submition.active {
-      background-color: var(--tp-heading-secondary);
-    }
-    .tptrack__submition i {
-      margin-left: 20px;
-    }
-    .tptrack__submition.active:hover {
-      background-color: #859a00;
-    }
-    .tpsign__pass > a,
-    .tpsign__account > a {
-      color: inherit;
-    }
-  </style>
+.tptrack__submition {
+  background: var(--tp-heading-primary);
+  border-radius: 6px;
+  color: var(--tp-common-white);
+  display: inline-block;
+  font-size: 16px;
+  font-weight: 600;
+  line-height: 1;
+  margin-bottom: 0;
+  padding: 22px 50px;
+  text-align: center;
+  touch-action: manipulation;
+  transition: all 0.3s ease 0s;
+  vertical-align: middle;
+  white-space: nowrap;
+  width: 100%;
+}
+.tptrack__submition:hover,
+.tptrack__submition.active {
+  background-color: var(--tp-heading-secondary);
+}
+.tptrack__submition i {
+  margin-left: 20px;
+}
+.tptrack__submition.active:hover {
+  background-color: #859a00;
+}
+.tpsign__pass > a,
+.tpsign__account > a {
+  color: inherit;
+}
+</style>
